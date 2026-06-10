23110f6280ac671548936848b07baaaae5c94534
kaixer-glitched <unmei917@gmail.com>
2026-06-10 22:04:48 +0800

    Initial Commit

---
+++ src/main/java/com/greedycalc/greedycalc/GreedyCalcApplication.java
@@ -0,0 +1,13 @@
+package com.greedycalc.greedycalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreedyCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreedyCalcApplication.class, args);
+    }
+
+}
