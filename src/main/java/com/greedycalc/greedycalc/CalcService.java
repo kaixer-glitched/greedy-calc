474a3276f9429a8a036420927e0499f801faed2f
kaixer-glitched <unmei917@gmail.com>
2026-06-13 23:18:08 +0800

    Added the main calculation logic in service.

--- src/main/java/com/greedycalc/greedycalc/CalcService.java
+++ src/main/java/com/greedycalc/greedycalc/CalcService.java
@@ -14,7 +14,7 @@ public class CalcService {
     // TODO: configure the application.properties for database.
     // TODO: create a way to send a proper DTO for the client.
 
-    public CalcResponseDTO toDTO(Calculation calculation) {
+    public CalcResponseDTO toResponseDTO(Calculation calculation) {
         CalcResponseDTO calcResponseDTO = new CalcResponseDTO();
         calcResponseDTO.setCalcId(calculation.getCalcId());
         calcResponseDTO.setOperation(calculation.getOperation());
@@ -26,7 +26,25 @@ public class CalcService {
 
     public CalcResponseDTO saveCalculation(Calculation calculation) {
         Calculation saved = calcRepository.save(calculation);
-        return toDTO(saved);
+        return toResponseDTO(saved);
     }
 
+    public CalcResponseDTO calculate(Calculation calculation) {
+        double val1 = calculation.getValue1();
+        double val2 = calculation.getValue2();
+        double result = switch (calculation.getOperation()) {
+            case ADD -> val1 + val2;
+            case SUBTRACT ->  val1 - val2;
+            case MULTIPLY -> val1 * val2 ;
+            case DIVIDE -> {
+                if (val2 == 0) throw new IllegalArgumentException("Cannot divide by zero");
+                yield val1 / val2;
+            }
+            default -> throw new IllegalArgumentException("Invalid operation");
+        };
+
+        calculation.setResult(result);
+        Calculation saved = calcRepository.save(calculation);
+        return toResponseDTO(saved);
+    }
 }
